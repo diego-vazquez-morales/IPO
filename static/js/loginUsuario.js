fa87121bf69154c4573a664ff27024b65b2731c7
AlejandroLarena <9305266@alumnos.ufv.es>
2025-11-15 14:00:54 +0100

    Modificación completa de HTML, CSS Y JS, de la pagina loginUsuario

--- static/js/loginUsuario.js
+++ static/js/loginUsuario.js
@@ -2,7 +2,7 @@
    Script de interacción para loginUsuario.html
 */
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   console.log('loginUsuario.js cargado');
   var form = document.querySelector('form');
   if (form) {
@@ -18,3 +18,24 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 });
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0f49bd',
+        'background-light': '#f6f7f8',
+        'background-dark': '#101922'
+      },
+      fontFamily: {
+        display: ['Inter', 'sans-serif']
+      },
+      borderRadius: {
+        DEFAULT: '0.25rem',
+        lg: '0.5rem',
+        xl: '0.75rem',
+        full: '9999px'
+      }
+    }
+  }
+}
